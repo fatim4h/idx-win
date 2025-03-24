b705dd276e5809fa07954342f855629d6f792a2d
administrator <admin@grt.my.id>
2025-03-24 18:50:58 +0000

    refactor: remove other parameters, idx doesnt support multiple params

--- idx-template.nix
+++ idx-template.nix
@@ -7,12 +7,12 @@
   bootstrap = ''
     mkdir -p "$WS_NAME"
     mkdir "$WS_NAME/.idx/"
-    tailscaleHostName=${tailscaleHostName} tailscaleAuthToken=${tailscaleAuthToken} winUser=${winUser} winPass=${winPass} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
+    tailscaleAuthToken=${tailscaleAuthToken} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
     nixfmt "$WS_NAME/.idx/dev.nix"
     cp -r ${./oem} "$WS_NAME/oem"
     cp ${./README.j2} "$WS_NAME/README.md"
-    env >> "$WS_NAME/env"
     chmod -R +w "$WS_NAME"
+    env >> "$WS_NAME/env"
     mv "$WS_NAME" "$out"
   '';
 }
